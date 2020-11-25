86bcfe121d87f8de37c8db30ad49eddf0f981dd2
User <mahmoodahmad786786gmail.com>
2020-11-25 21:59:31 +0500

    FloatingActionButton || change color-56-2

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,54 @@ import 'MainPage.dart';
 
 void main() => runApp(SplashScreen());
 
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MySplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => BmiCalculatur())));
+
+  }
 FloatingActionButton.extended(
   backgroundColor: const Color(0xff03dac6),
-  foregroundColor: Colors.black,
+  foregroundColor: Colors.blue,
   onPressed: () {
     // Respond to button press
   },
   icon: Icon(Icons.add),
   label: Text('EXTENDED'),
 )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/bmi1.png', height: 250.0, width: 250,),
+
+          SizedBox( height: 30.0),
+
+          SpinKitRipple(color: Colors.black, size: 150.0, borderWidth: 10.0,),
+        ],
+      ),
+    );
+  }
+}
