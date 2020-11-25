103493ce54de9807e57023d146e8456e19a30a3b
User <mahmoodahmad786786gmail.com>
2020-11-25 21:41:24 +0500

    FloatingActionButton ||

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,12 @@ import 'MainPage.dart';
 
 void main() => runApp(SplashScreen());
 
-MaterialApp(
-  title: title,
-  theme: ThemeData(
-    // Define the default brightness and colors.
-    brightness: Brightness.dark,
-    primaryColor: Colors.lightBlue[800],
-    accentColor: Colors.cyan[600],
-
-    // Define the default font family.
-    fontFamily: 'Georgia',
-
-    // Define the default TextTheme. Use this to specify the default
-    // text styling for headlines, titles, bodies of text, and more.
-    textTheme: TextTheme(
-      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-      headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-      bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-    ),
-  )
-);
+FloatingActionButton.extended(
+  backgroundColor: const Color(0xff03dac6),
+  foregroundColor: Colors.black,
+  onPressed: () {
+    // Respond to button press
+  },
+  icon: Icon(Icons.add),
+  label: Text('EXTENDED'),
+)
