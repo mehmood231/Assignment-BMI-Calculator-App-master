71eef3acd515c450135a68f0c6141a5fe7197a91
User <mahmoodahmad786786gmail.com>
2020-11-25 22:16:08 +0500

    navigation process-72

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,60 @@ import 'MainPage.dart';
 
 
 void main() => runApp(SplashScreen());
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: PreferredSize(
-      child: BmiAppBar(),
-      preferredSize: Size.fromHeight(appBarHeight(context)),
-    ),
-    body: ...
-  );
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MySplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => BmiCalculatur())))
+  }
+FloatingActionButton.extended(
+  backgroundColor: const Color(0xff03dac6),
+  foregroundColor: Colors.blue,
+  onPressed: () {
+    // Respond to button press
+  },
+  icon: Icon(Icons.add),
+  label: Text('EXTENDED'),
+)
+  @override
+  void initState() {
+    super.initState();
+    _bordersAnimation = BorderRadiusTween( //create BorderRadiusTween
+      begin: BorderRadius.circular(8.0),
+      end: BorderRadius.circular(50.0),
+    ).animate(CurvedAnimation(
+      parent: widget.submitAnimationController,
+      curve: Interval(0.0, 0.07), //specify interval from 0 to 7%
+    ));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/bmi1.png', height: 250.0, width: 250,),
+
+          SizedBox( height: 30.0),
+
+          SpinKitRipple(color: Colors.black, size: 150.0, borderWidth: 10.0,),
+        ],
+      ),
+    );
+  }
+}
